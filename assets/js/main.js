3f723988ee6c5457fc4f62c5dbdef1c2955b7f6a
Tatyana404 <kot.kate2020@gmail.com>
2022-05-12 23:22:21 +0300

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,13 @@
-const swiper = new Swiper(".swiper-container", {
+const swiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button-next",
-    prevEl: ".slider-button-prev",
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev'
   },
   // And if we need scrollbar
   scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
-
-
-
-
+    el: '.swiper-scrollbar'
+  }
+})
